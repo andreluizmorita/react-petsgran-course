b5d5f34963871af68e0677e46463fedb3463351f
Andre Morita <andreluizmorita@gmail.com>
2020-11-02 09:03:47 -0300

    feat: stats page

--- src/pages/User/UserStats.js
+++ src/pages/User/UserStats.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
-const UserStats = () =>{
-  return (
-    <h1>UserStats</h1>
-  );
-}
+import Head from '../../components/Helper/Head';
+import Loading from '../../components/Helper/Loading';
+import Error from '../../components/Helper/Error';
+
+import useFetch from '../../hooks/useFetch';
+
+import UserStatsGraphs from './UserStatsGraphs';
+
+import { STATS_GET } from '../../services/api';
+
+const UserStats = () => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
+};
+
 export default UserStats;
